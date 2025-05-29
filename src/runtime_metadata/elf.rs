b414196c1ef7bec69bb90b8c08856f210623482f
Millz <millzyyt08@gmail.com>
2025-05-29 23:47:28 +0100

    move errors into its own file

--- src/runtime_metadata/elf.rs
+++ src/runtime_metadata/elf.rs
@@ -10,56 +10,17 @@
 
 use super::*;
 use crate::global_metadata::{GenericParameterIndex, GlobalMetadata, TypeDefinitionIndex};
-use bad64::{disasm, DecodeError, Imm, Instruction, Op, Operand, Reg};
+use crate::runtime_metadata::errors::{Il2CppBinaryError, Result};
+use bad64::{disasm, Imm, Instruction, Op, Operand, Reg};
 use binread::{BinRead, BinReaderExt};
 use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
 use object::read::elf::ElfFile64;
 use object::{Endianness, Object, ObjectSection, ObjectSegment, ObjectSymbol, RelocationEncoding, RelocationTarget};
 use std::collections::HashMap;
-use std::io::{self, Cursor};
+use std::io::Cursor;
 use std::str;
-use thiserror::Error;
 
 pub type Elf<'data> = ElfFile64<'data, Endianness>;
-
-#[derive(Error, Debug, Clone, Copy)]
-#[error("error disassembling code")]
-pub struct DisassembleError;
-
-#[derive(Error, Debug)]
-pub enum Il2CppBinaryError {
-    #[error("error disassembling code")]
-    Disassemble(DecodeError),
-
-    #[error("failed to convert virtual address {0:#016x}")]
-    VAddrConv(u64),
-
-    #[error("could not find il2cpp_init symbol in elf")]
-    MissingIl2CppInit,
-
-    #[error("could not find indirect branch in Runtime::Init")]
-    MissingBlr,
-
-    #[error("could not find registration function")]
-    MissingRegistration,
-
-    #[error("invalid Il2CppType with type {0}")]
-    InvalidType(u8),
-
-    #[error(transparent)]
-    Io(#[from] io::Error),
-
-    #[error(transparent)]
-    BinaryDeserialize(#[from] binread::Error),
-
-    #[error(transparent)]
-    Utf8(#[from] str::Utf8Error),
-
-    #[error(transparent)]
-    Elf(#[from] object::Error),
-}
-
-type Result<T> = std::result::Result<T, Il2CppBinaryError>;
 
 pub fn strlen(data: &[u8], offset: usize) -> usize {
     let mut len = 0;
---
+++ src/runtime_metadata/errors.rs
@@ -0,0 +1,45 @@
+﻿use std::io;
+use thiserror::Error;
+
+#[cfg(feature = "elf")]
+use bad64::DecodeError;
+
+pub type Result<T> = std::result::Result<T, Il2CppBinaryError>;
+
+#[derive(Error, Debug, Clone, Copy)]
+#[error("error disassembling code")]
+pub struct DisassembleError;
+
+#[derive(Error, Debug)]
+pub enum Il2CppBinaryError {
+    #[cfg(feature = "elf")]
+    #[error("error disassembling code")]
+    Disassemble(DecodeError),
+
+    #[error("failed to convert virtual address {0:#016x}")]
+    VAddrConv(u64),
+
+    #[error("could not find il2cpp_init symbol in elf")]
+    MissingIl2CppInit,
+
+    #[error("could not find indirect branch in Runtime::Init")]
+    MissingBlr,
+
+    #[error("could not find registration function")]
+    MissingRegistration,
+
+    #[error("invalid Il2CppType with type {0}")]
+    InvalidType(u8),
+
+    #[error(transparent)]
+    Io(#[from] io::Error),
+
+    #[error(transparent)]
+    BinaryDeserialize(#[from] binread::Error),
+
+    #[error(transparent)]
+    Utf8(#[from] std::str::Utf8Error),
+
+    #[error(transparent)]
+    Elf(#[from] object::Error),
+}
